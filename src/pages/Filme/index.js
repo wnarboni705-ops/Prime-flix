4ec4c173bafb824830febfc5f7dac26e121f680a
Wallace Narboni <wnarboni705@gmail.com>
2026-07-28 20:15:59 -0300

    Wallace narboni

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,8 @@
+
+function Filme(){
+    return(
+        <div>
+            <h1>Seja bem vindo aos filmes</h1>
+        </div>
+    )
+}
---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+function Home (){
+    return(
+        <div>
+            <h1>Bem vindo a HOME do projeto</h1>
+        </div>
+    )
+}
+
+export default Home;
